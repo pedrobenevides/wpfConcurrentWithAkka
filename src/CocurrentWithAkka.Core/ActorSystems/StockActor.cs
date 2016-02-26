b102a90e2d72bc98258bb877e8971e9c7d1c1dd7
pedrobenevides86 <pedrobenevidesb@gmail.com>
2016-02-25 21:24:38 -0300

    StockActor

--- src/CocurrentWithAkka.Core/ActorSystems/StockActor.cs
+++ src/CocurrentWithAkka.Core/ActorSystems/StockActor.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using Akka.Actor;
+using Akka.DI.Core;
+using CocurrentWithAkka.Core.ActorModels;
 using CocurrentWithAkka.Core.Messages;
 
 namespace CocurrentWithAkka.Core.ActorSystems
@@ -7,15 +9,28 @@ namespace CocurrentWithAkka.Core.ActorSystems
     public class StockActor : ReceiveActor
     {
         private readonly string _stockSymbol;
-        private readonly HashSet<IActorRef> _subscribers; 
+        private readonly HashSet<IActorRef> _subscribers;
+        private readonly IActorRef _priceLookupActor;
+        private decimal _stockPrice;
 
         public StockActor(string stockSymbol)
         {
             _stockSymbol = stockSymbol;
             _subscribers = new HashSet<IActorRef>();
+            _priceLookupActor = Context.ActorOf(Context.DI().Props<StockPriceLookupActor>());
 
             Receive<SubscribeToNewStockPriceMessage>(message => _subscribers.Add(message.Subscriber));
             Receive<UnsubscribeFromNewStockPriceMessage>(message => _subscribers.Remove(message.Subscriber));
+
+            Receive<RefreshStockPriceMessage>(message => _priceLookupActor.Tell(message.StockSymbol));
+            Receive<UpdatedStockPriceMessage>(message =>
+            {
+                _stockPrice = message.Price;
+                var stockPriceMessage = new StockPriceMessage(_stockSymbol, _stockPrice, message.Date);
+
+                foreach (var subscriber in _subscribers)
+                    subscriber.Tell(stockPriceMessage);
+            });
         }
     }
 }
---
+++ src/CocurrentWithAkka.Core/Messages/StockPriceMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CocurrentWithAkka.Core.Messages
+{
+    internal class StockPriceMessage
+    {
+        public DateTime Date { get; set; }
+        public decimal StockPrice { get; private set;}
+        public string StockSymbol { get; private set;}
+
+        public StockPriceMessage(string stockSymbol, decimal stockPrice, DateTime date)
+        {
+            StockSymbol = stockSymbol;
+            StockPrice = stockPrice;
+            Date = date;
+        }
+    }
+}
